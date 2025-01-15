5d352230b4bc22b8b9f576b183c24b0984895174
Khouloud <marzoukikhouloud4@gmail.com>
2025-01-15 22:31:50 +0100

    Ajoutez des fichiers projet.

---
+++ calcul2/Form1.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Btn_9 = New Button()
+        Btn_4 = New Button()
+        Btn_1 = New Button()
+        btn_virgule = New Button()
+        Btn_8 = New Button()
+        Btn_5 = New Button()
+        Btn_2 = New Button()
+        Btn_7 = New Button()
+        Btn_0 = New Button()
+        Btn_6 = New Button()
+        Btn_3 = New Button()
+        BTN_plus = New Button()
+        Btn_minus = New Button()
+        Btn_div = New Button()
+        Btn_equal = New Button()
+        Btn_foix = New Button()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Btn_9
+        ' 
+        Btn_9.Location = New Point(78, 102)
+        Btn_9.Name = "Btn_9"
+        Btn_9.Size = New Size(94, 49)
+        Btn_9.TabIndex = 0
+        Btn_9.Text = "9"
+        Btn_9.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_4
+        ' 
+        Btn_4.Location = New Point(78, 190)
+        Btn_4.Name = "Btn_4"
+        Btn_4.Size = New Size(94, 50)
+        Btn_4.TabIndex = 1
+        Btn_4.Text = "4"
+        Btn_4.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_1
+        ' 
+        Btn_1.Location = New Point(78, 275)
+        Btn_1.Name = "Btn_1"
+        Btn_1.Size = New Size(94, 45)
+        Btn_1.TabIndex = 2
+        Btn_1.Text = "1"
+        Btn_1.UseVisualStyleBackColor = True
+        ' 
+        ' btn_virgule
+        ' 
+        btn_virgule.Location = New Point(78, 351)
+        btn_virgule.Name = "btn_virgule"
+        btn_virgule.Size = New Size(94, 46)
+        btn_virgule.TabIndex = 3
+        btn_virgule.Text = "."
+        btn_virgule.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_8
+        ' 
+        Btn_8.Location = New Point(259, 102)
+        Btn_8.Name = "Btn_8"
+        Btn_8.Size = New Size(94, 49)
+        Btn_8.TabIndex = 4
+        Btn_8.Text = "8"
+        Btn_8.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_5
+        ' 
+        Btn_5.Location = New Point(259, 190)
+        Btn_5.Name = "Btn_5"
+        Btn_5.Size = New Size(94, 50)
+        Btn_5.TabIndex = 5
+        Btn_5.Text = "5"
+        Btn_5.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_2
+        ' 
+        Btn_2.Location = New Point(259, 275)
+        Btn_2.Name = "Btn_2"
+        Btn_2.Size = New Size(94, 45)
+        Btn_2.TabIndex = 6
+        Btn_2.Text = "2"
+        Btn_2.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_7
+        ' 
+        Btn_7.Location = New Point(433, 102)
+        Btn_7.Name = "Btn_7"
+        Btn_7.Size = New Size(94, 49)
+        Btn_7.TabIndex = 7
+        Btn_7.Text = "7"
+        Btn_7.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_0
+        ' 
+        Btn_0.Location = New Point(259, 351)
+        Btn_0.Name = "Btn_0"
+        Btn_0.Size = New Size(94, 46)
+        Btn_0.TabIndex = 8
+        Btn_0.Text = "0"
+        Btn_0.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_6
+        ' 
+        Btn_6.Location = New Point(433, 190)
+        Btn_6.Name = "Btn_6"
+        Btn_6.Size = New Size(94, 50)
+        Btn_6.TabIndex = 9
+        Btn_6.Text = "6"
+        Btn_6.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_3
+        ' 
+        Btn_3.Location = New Point(433, 275)
+        Btn_3.Name = "Btn_3"
+        Btn_3.Size = New Size(94, 45)
+        Btn_3.TabIndex = 10
+        Btn_3.Text = "3"
+        Btn_3.UseVisualStyleBackColor = True
+        ' 
+        ' BTN_plus
+        ' 
+        BTN_plus.Enabled = False
+        BTN_plus.Location = New Point(632, 102)
+        BTN_plus.Name = "BTN_plus"
+        BTN_plus.Size = New Size(94, 49)
+        BTN_plus.TabIndex = 11
+        BTN_plus.TabStop = False
+        BTN_plus.Text = "+"
+        BTN_plus.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_minus
+        ' 
+        Btn_minus.Location = New Point(632, 190)
+        Btn_minus.Name = "Btn_minus"
+        Btn_minus.Size = New Size(94, 50)
+        Btn_minus.TabIndex = 12
+        Btn_minus.Text = "-"
+        Btn_minus.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_div
+        ' 
+        Btn_div.Location = New Point(632, 275)
+        Btn_div.Name = "Btn_div"
+        Btn_div.Size = New Size(94, 45)
+        Btn_div.TabIndex = 13
+        Btn_div.Text = "/"
+        Btn_div.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_equal
+        ' 
+        Btn_equal.Location = New Point(433, 351)
+        Btn_equal.Name = "Btn_equal"
+        Btn_equal.Size = New Size(94, 46)
+        Btn_equal.TabIndex = 14
+        Btn_equal.Text = "="
+        Btn_equal.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_foix
+        ' 
+        Btn_foix.Location = New Point(632, 351)
+        Btn_foix.Name = "Btn_foix"
+        Btn_foix.Size = New Size(94, 46)
+        Btn_foix.TabIndex = 15
+        Btn_foix.Text = "*"
+        Btn_foix.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.ForeColor = SystemColors.MenuHighlight
+        TextBox1.Location = New Point(47, 33)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(661, 27)
+        TextBox1.TabIndex = 16
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(Btn_foix)
+        Controls.Add(Btn_equal)
+        Controls.Add(Btn_div)
+        Controls.Add(Btn_minus)
+        Controls.Add(BTN_plus)
+        Controls.Add(Btn_3)
+        Controls.Add(Btn_6)
+        Controls.Add(Btn_0)
+        Controls.Add(Btn_7)
+        Controls.Add(Btn_2)
+        Controls.Add(Btn_5)
+        Controls.Add(Btn_8)
+        Controls.Add(btn_virgule)
+        Controls.Add(Btn_1)
+        Controls.Add(Btn_4)
+        Controls.Add(Btn_9)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Btn_9 As Button
+    Friend WithEvents Btn_4 As Button
+    Friend WithEvents Btn_1 As Button
+    Friend WithEvents btn_virgule As Button
+    Friend WithEvents Btn_8 As Button
+    Friend WithEvents Btn_5 As Button
+    Friend WithEvents Btn_2 As Button
+    Friend WithEvents Btn_7 As Button
+    Friend WithEvents Btn_0 As Button
+    Friend WithEvents Btn_6 As Button
+    Friend WithEvents Btn_3 As Button
+    Friend WithEvents BTN_plus As Button
+    Friend WithEvents Btn_minus As Button
+    Friend WithEvents Btn_div As Button
+    Friend WithEvents Btn_equal As Button
+    Friend WithEvents Btn_foix As Button
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
---
+++ calcul2/Form1.vb
@@ -0,0 +1,161 @@
+﻿Public Class Form1
+    Dim A As String = ""
+    Dim B As String = ""
+    Dim op As String = ""
+
+
+    Private Sub Btn_9_Click(sender As Object, e As EventArgs) Handles Btn_9.Click
+        If op = "" Then
+            A = A + "9"
+        Else
+            B = B + "9"
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+
+    End Sub
+
+    Private Sub Btn_1_Click(sender As Object, e As EventArgs) Handles Btn_1.Click
+        If op = "" Then
+            A = A + "1"
+        Else
+            B = B + "1"
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+
+    End Sub
+
+    Private Sub Btn_3_Click(sender As Object, e As EventArgs) Handles Btn_3.Click
+        If op = "" Then
+            A = A + "3"
+        Else
+            B = B + "3"
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+    End Sub
+
+    Private Sub Btn_6_Click(sender As Object, e As EventArgs) Handles Btn_6.Click
+        If op = "" Then
+            A = A + "6"
+        Else
+            B = B + "6"
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+    End Sub
+
+    Private Sub Btn_0_Click(sender As Object, e As EventArgs) Handles Btn_0.Click
+        If op = "" Then
+            A = A + "0"
+        Else
+            B = B + "0"
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+    End Sub
+
+    Private Sub Btn_4_Click(sender As Object, e As EventArgs) Handles Btn_4.Click
+        If op = "" Then
+            A = A + "4"
+        Else
+            B = B + "4"
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+    End Sub
+
+    Private Sub Btn_7_Click(sender As Object, e As EventArgs) Handles Btn_7.Click
+        If op = "" Then
+            A = A + "7"
+        Else
+            B = B + "7"
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+    End Sub
+
+    Private Sub Btn_8_Click(sender As Object, e As EventArgs) Handles Btn_8.Click
+        If op = "" Then
+            A = A + "8"
+        Else
+            B = B + "8"
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+    End Sub
+
+    Private Sub Btn_5_Click(sender As Object, e As EventArgs) Handles Btn_5.Click
+        If op = "" Then
+            A = A + "5"
+        Else
+            B = B + "5"
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+    End Sub
+
+    Private Sub Btn_2_Click(sender As Object, e As EventArgs) Handles Btn_2.Click
+        If op = "" Then
+            A = A + "2"
+        Else
+            B = B + "2"
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+    End Sub
+
+    Private Sub NewMethod()
+        TextBox1.Text = A + "" + op + "" + B
+    End Sub
+
+    Private Sub Btn_equal_Click(sender As Object, e As EventArgs) Handles Btn_equal.Click
+        Dim test = True
+        Dim res As Double
+        If IsNumeric(A) And IsNumeric(B) And Not op = "" Then
+            Dim va = Integer.Parse(A)
+            Dim vb = Integer.Parse(B)
+            If op = "+ " Then
+                res = va + vb
+            ElseIf op = "-" Then
+                res = va - vb
+            ElseIf op = "*" Then
+                res = va * vb
+            ElseIf op = "/" Then
+                res = va / vb
+            Else MessageBox.Show("A et B  doivent etre des entiers")
+                test = False
+            End If
+            Dim teste As Boolean = True
+            If teste Then
+                TextBox1.Text = TextBox1.Text + "=" + res.ToString()
+            End If
+        Else
+            MessageBox.Show("entiee non valide")
+        End If
+    End Sub
+
+    Private Sub btn_virgule_Click(sender As Object, e As EventArgs) Handles btn_virgule.Click
+        If op = "" Then
+            A = A + "."
+        Else
+            B = B + "."
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+    End Sub
+
+
+    Private Sub Btn_minus_Click(sender As Object, e As EventArgs) Handles Btn_minus.Click
+        If Not A = "" Then
+            op = "-"
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+
+    End Sub
+
+    Private Sub Btn_foix_Click(sender As Object, e As EventArgs) Handles Btn_foix.Click
+        If Not A = "" Then
+            op = "*"
+        End If
+        TextBox1.Text = A + "" + op + "" + B
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
